023cc87e31602fa57453259c2d958dc253b4737a
entero.vipun <entero.vipunen@protonmail.com>
2021-07-21 23:38:02 -0500

    merged a few bug fixes and PRs from Chia backlog

--- dogechia/cmds/wallet.py
+++ dogechia/cmds/wallet.py
@@ -1,5 +1,8 @@
 import click
+import sys
+from typing import Any, Dict
 
+from decimal import Decimal
 
 @click.group("wallet", short_help="Manage your wallet")
 def wallet_cmd() -> None:
@@ -88,6 +91,7 @@ def send_cmd(
     asyncio.run(execute_with_wallet(wallet_rpc_port, fingerprint, extra_params, send))
 
 
+
 @wallet_cmd.command("show", short_help="Show wallet information")
 @click.option(
     "-wp",
@@ -121,6 +125,126 @@ def get_address_cmd(wallet_rpc_port: int, id, fingerprint: int) -> None:
 
     asyncio.run(execute_with_wallet(wallet_rpc_port, fingerprint, extra_params, get_address))
 
+@wallet_cmd.group("create", short_help="Create new wallets")
+def wallet_create_cmd():
+    pass
+
+
+@wallet_create_cmd.command("coloured-coin", short_help="Create a coloured coin wallet")
+@click.option(
+    "-wp",
+    "--wallet-rpc-port",
+    help="Set the port where the Wallet is hosting the RPC interface. See the rpc_port under wallet in config.yaml",
+    type=int,
+    default=None,
+)
+@click.option("-c", "--colour-id", help="Id of the colour for the new wallet", type=str)
+@click.option("-a", "--amount", help="How much dogechia to destroy to create this coloured coin, in TXDG/XDG", type=str)
+@click.option(
+    "-m",
+    "--fee",
+    help="Set the fees for the transaction, in XDG",
+    type=str,
+    default="0",
+    show_default=True,
+    required=True,
+)
+@click.option("-f", "--fingerprint", help="Set the fingerprint to specify which wallet to use", type=int)
+def create_coloured_coin_cmd(wallet_rpc_port: int, colour_id: str, amount: str, fee: str, fingerprint: int) -> None:
+    if not colour_id and not amount:
+        print(
+            (
+                "You must use --amount to create a new coloured coin or --colour-id "
+                "to create a wallet of an existing colour, but at least one."
+            )
+        )
+        sys.exit(1)
+    if colour_id and amount:
+        print(
+            (
+                "You can use --amount to create a new coloured coin or --colour-id "
+                "to create a wallet of an existing colour, but not both."
+            )
+        )
+        sys.exit(1)
+
+    import asyncio
+    from .wallet_funcs import execute_with_wallet, create_new_wallet
+    from dogechia.cmds.units import units
+    from dogechia.util.ints import uint64
+
+    final_fee = uint64(int(Decimal(fee) * units["dogechia"]))
+    data: Dict[str, Any] = {"fee": final_fee}
+    if colour_id:
+        data["mode"] = "existing"
+        data["colour"] = colour_id
+    else:
+        data["mode"] = "new"
+        final_amount = uint64(int(Decimal(amount) * units["dogechia"]))
+        data["amount"] = final_amount
+    extra_params = {"wallet_type": "cc_wallet", "data": data}
+    asyncio.run(execute_with_wallet(wallet_rpc_port, fingerprint, extra_params, create_new_wallet))
+
+
+@wallet_create_cmd.command("rate-limited-admin", short_help="Create a rate limited admin wallet")
+@click.option(
+    "-wp",
+    "--wallet-rpc-port",
+    help="Set the port where the Wallet is hosting the RPC interface. See the rpc_port under wallet in config.yaml",
+    type=int,
+    default=None,
+)
+@click.option("-i", "--interval", help="Spending interval length (Number of blocks).", type=int, required=True)
+@click.option("-l", "--limit", help="Spendable amount per interval, in TXDG/XDG.", type=str, required=True)
+@click.option("-a", "--amount", help="Amount for initial coin. in TXDG/XDG.", type=str, required=True)
+@click.option(
+    "-m",
+    "--fee",
+    help="Set the fees for the transaction, in XDG",
+    type=str,
+    default="0",
+    show_default=True,
+    required=True,
+)
+@click.option("--public_key", "-p", default=None, help="Enter the pk in hex", type=str, required=True)
+@click.option("-f", "--fingerprint", help="Set the fingerprint to specify which wallet to use", type=int)
+def create_rate_limited_admin_cmd(
+    wallet_rpc_port: int, interval: int, limit: str, amount: str, fee: str, public_key: str, fingerprint: int
+) -> None:
+    import asyncio
+    from .wallet_funcs import execute_with_wallet, create_new_wallet
+    from dogechia.cmds.units import units
+    from dogechia.util.ints import uint64
+
+    final_fee = uint64(int(Decimal(fee) * units["dogechia"]))
+    final_amount = uint64(int(Decimal(amount) * units["dogechia"]))
+    final_limit = uint64(int(Decimal(limit) * units["dogechia"]))
+    data: Dict[str, Any] = {
+        "fee": final_fee,
+        "amount": final_amount,
+        "pubkey": public_key,
+        "limit": final_limit,
+        "rl_type": "admin",
+        "interval": interval,
+    }
+    extra_params = {"wallet_type": "rl_wallet", "data": data}
+    asyncio.run(execute_with_wallet(wallet_rpc_port, fingerprint, extra_params, create_new_wallet))
+
+@wallet_create_cmd.command("rate-limited-user", short_help="Create a rate limited user wallet")
+@click.option(
+    "-wp",
+    "--wallet-rpc-port",
+    help="Set the port where the Wallet is hosting the RPC interface. See the rpc_port under wallet in config.yaml",
+    type=int,
+    default=None,
+)
+@click.option("-f", "--fingerprint", help="Set the fingerprint to specify which wallet to use", type=int)
+def create_rate_limited_user_cmd(wallet_rpc_port: int, fingerprint: int) -> None:
+    import asyncio
+    from .wallet_funcs import execute_with_wallet, create_new_wallet
+
+    extra_params = {"wallet_type": "rl_wallet", "data": {"rl_type": "user"}}
+    asyncio.run(execute_with_wallet(wallet_rpc_port, fingerprint, extra_params, create_new_wallet))
 
 @wallet_cmd.command(
     "delete_unconfirmed_transactions", short_help="Deletes all unconfirmed transactions for this wallet ID"
