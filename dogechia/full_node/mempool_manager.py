fc3484a70c0427d1d6b7cb1e251d1c835fb73b3e
entero.vipun <entero.vipunen@protonmail.com>
2021-07-21 23:00:33 -0500

    fixed some typos

--- dogechia/full_node/mempool_manager.py
+++ dogechia/full_node/mempool_manager.py
@@ -57,7 +57,7 @@ class MempoolManager:
         self.coin_store = coin_store
 
         # The fee per cost must be above this amount to consider the fee "nonzero", and thus able to kick out other
-        # transactions. This prevents spam. This is equivalent to 0.055 XCH per block, or about 0.00005 XCH for two
+        # transactions. This prevents spam. This is equivalent to 0.055 XDG per block, or about 0.00005 XDG for two
         # spends.
         self.nonzero_fee_minimum_fpc = 5
 
